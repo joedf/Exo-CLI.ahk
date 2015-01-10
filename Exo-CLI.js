717e31240ae65bed5f568b7ea29ad86caedc2636
Joe DF <joedf@live.ca>
2015-01-09 23:19:29 -0500

    Thanks Lexikos - Optimized I/O
    No need to reimplement to whole cmd experience

--- Exo-CLI.js
+++ Exo-CLI.js
@@ -1,92 +1,36 @@
 ﻿//exocli.js test by joedf 19:34 2015-01-09
+//many thanks to Lexikos for StdOut/StdIn (2015-01-10)
 
 var hCmd = DllCall("AllocConsole");
+
+var StdOut = FileOpen("*", "w");
+var StdIn = FileOpen("*", "r");
+
 function print(t) {
-	h = DllCall("GetStdHandle", "UInt", -11, "UPtr");
-	return DllCall("WriteConsole", "UPtr", h, "Str", t, "UInt", StrLen(t), "UInt*", 0, "UInt", 0);
-}
-function getch() {
-	//return DllCall("msvcrt.dll\_getch","int"); //not working ...
-	var k = Input("L1","{BackSpace}{Enter}{Up}{Down}{Left}{Right}");
-	var e = ErrorLevel;
-	if (InStr(e,"EndKey:") != 0) {
-		return e;
-	} else {
-		return k;
-	}
+    StdOut.Write(t);
+    StdOut.__Handle; // Flush the buffer.
 }
 
 print("AutoHotkey v" + A_AhkVersion + " with Exo\n>> ");
-
-var StackHistory = [];
-var StackHistoryPos = 0;
-var CurrentLine = "";
-var CurrentPos = 0;
-
 for(;;) {
-	var key = getch();
-	if (InStr(key,"Enter") != 0) {
-		if (InStr(CurrentLine,"Quit") != 0) {
-			print("\nOk. Goob bye!");
-			break;
-		}
-		if (StrLen(Trim(CurrentLine))) {
-			print("\n");
-			StackHistory.push(CurrentLine);
-			StackHistoryPos = StackHistory.length;
-			try {
-				var eval_start = (new Date()).getTime();
-				eval(CurrentLine);
-				var eval_end = (new Date()).getTime();
-				var eval_diff = (eval_end - eval_start);
-				//Show execution time
-				//print("\nExecution time : " + (eval_diff/1000) + ' seconds');
-			} catch( e ) {
-				print(e.name+" : "+e.message);
-			}
-		}
-		print("\n>> ");
-		CurrentLine = "";
-	} else {
-		if (SubStr(key,1,7) == "EndKey:") {
-			var k = SubStr(key,8);
-			switch(k) {
-				case "Backspace":
-					if (StrLen(CurrentLine) >= 1) {
-						print("\b \b");
-						CurrentLine = StringTrimRight(CurrentLine,1);
-						CurrentPos-=1;
-					}
-					break;
-				case "Up":
-					if (StackHistoryPos >0) {
-						for(i=0;i<CurrentPos;i++) {
-							print("\b")
-						}
-						StackHistoryPos-=1;
-						CurrentLine=StackHistory[StackHistoryPos];
-						print(">> "+CurrentLine);
-						CurrentPos=StrLen(CurrentLine);
-					} else if (StackHistory.length < 1) {
-						print("\nError : No lines currently in StackHistory[]\n>> ");
-					}
-					break;
-				case "Down":
-					break;
-				case "Left":
-					//print("\b");
-					break;
-				case "Right":
-					break;
-				default:
-					print('\nUnsupported key : "'+k+'"\n>> ');
-			}
-		} else {
-			CurrentLine = CurrentLine + key;
-			print(key);
-			CurrentPos += 1;
+    var str = RTrim(StdIn.ReadLine(), "\r\n");
+    if (str.toLowerCase() == "quit") {
+        print("\nOk. Good bye!");
+        break;
+    } else if (StrLen(Trim(str))) {
+		print("\n");
+		try {
+			var eval_start = (new Date()).getTime();
+			eval(str);
+			var eval_end = (new Date()).getTime();
+			var eval_diff = (eval_end - eval_start);
+			//Show execution time
+			//print("\nExecution time : " + (eval_diff/1000) + ' seconds');
+		} catch( e ) {
+			print(e.name+" : "+e.message);
 		}
 	}
+    print("\n>> ");
 }
 
 Sleep(1000);
